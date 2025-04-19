4ba7e76e5ac491d25cc11da2003943ae7820671e
Banupriya Rajendran <banupriya658@gmail.com>
2025-04-19 08:50:45 +0200

    fix: subnet delegation revert

--- infrastructure/modules/container_app/main.tf
+++ infrastructure/modules/container_app/main.tf
@@ -3,15 +3,7 @@ resource "azurerm_subnet" "app" {
   name                 = "teqwerk-app-subnet-westeurop-01"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
-  address_prefixes     = ["10.0.0.0/21"]
-  
-  # delegation {
-  #   name = "delegation"
-  #   service_delegation {
-  #     name    = "Microsoft.App/environments"
-  #     actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-  #   }
-  # }
+  address_prefixes     = ["10.0.1.0/22"]
 }
 
 resource "azurerm_container_app_environment" "main" {
